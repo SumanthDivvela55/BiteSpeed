185b5fdb4f6571abe7609a9839c34d62e517e916
SumanthDivvela55 <131939264+SumanthDivvela55@users.noreply.github.com>
2025-02-09 18:38:32 +0530

    modified mongo url

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 require('dotenv').config()
 
 
-mongoose.connect(`${process.env.MONGO_URL}/bitespeed2`, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`${process.env.MONGO_URL}`, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB"))
 
 
